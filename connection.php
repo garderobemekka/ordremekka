43864569c664476730f2b693ec0f1fdd265512eb
garderobemekka <124746401+garderobemekka@users.noreply.github.com>
2023-02-08 11:01:25 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,29 +1,25 @@
 <?php
 
 session_start();
+function db_connect() {
+    static $connection;
 	$username	= "ordremeekka";   
 	$password	= "Gme1234567890987654321";
 	$dbname		= "gme";
 	$host		= "ordremekka.mysql.database.azure.com";
+
+    
+       if(!isset($connection)) { 
        $connection = mysqli_init();
-    	mysqli_ssl_set($connection,NULL,NULL, "https://ordremekka.azurewebsites.net/assets/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-    mysqli_real_connect($connection, $host, $username, $password, $dbname, 3306, MYSQLI_CLIENT_SSL);
-	
-if ($connection->connect_error) {
-  die("Connection failed: " . $connection->connect_error);
-}
-echo "Connected successfully";
-	
-	
-	
-//     if(!isset($connection)) { 
-     
+        mysqli_ssl_set($connection,NULL,NULL, "https://ordremekka.azurewebsites.net/assets/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+        mysqli_real_connect($connection, $host, $username, $password, $dbname, 3306, MYSQLI_CLIENT_SSL);
         
-//     if($connection === false) {
-//         return mysqli_connect_error(); 
-//     }
-//     return $connection;
-// }
+    }
+    if($connection === false) {
+        return mysqli_connect_error(); 
+    }
+    return $connection;
+}
 
 // function db_query($query) {
 //     $connection = db_connect();
