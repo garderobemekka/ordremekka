6473fc5e344abf7a70e8639c98c987b71493fb1a
garderobemekka <124746401+garderobemekka@users.noreply.github.com>
2023-02-08 09:58:47 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -8,38 +8,45 @@ function db_connect() {
 	$host		= "ordremekka.mysql.database.azure.com";
        $connection = mysqli_init();
     	mysqli_ssl_set($connection,NULL,NULL, "https://ordremekka.azurewebsites.net/assets/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-    
-
-    if(!isset($connection)) { 
+    mysqli_real_connect($connection, $host, $username, $password, $dbname, 3306, MYSQLI_CLIENT_SSL);
+	
+if ($connection->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+	
+	
+	
+//     if(!isset($connection)) { 
      
-        mysqli_real_connect($connection, $host, $username, $password, $dbname, 3306, MYSQLI_CLIENT_SSL);
-    if($connection === false) {
-        return mysqli_connect_error(); 
-    }
-    return $connection;
-}
+        
+//     if($connection === false) {
+//         return mysqli_connect_error(); 
+//     }
+//     return $connection;
+// }
 
-function db_query($query) {
-    $connection = db_connect();
-    $result = mysqli_query($connection,$query);
-    return $result;
-}
+// function db_query($query) {
+//     $connection = db_connect();
+//     $result = mysqli_query($connection,$query);
+//     return $result;
+// }
 
-function db_error() {
-    $connection = db_connect();
-    return mysqli_error($connection);
-}
+// function db_error() {
+//     $connection = db_connect();
+//     return mysqli_error($connection);
+// }
 
-$connect = db_connect();
+// $connect = db_connect();
 
-if (!isset($_SESSION['token'])) {
-    $token = md5(uniqid(rand(), TRUE));
-    $_SESSION['token'] = $token;
-    $_SESSION['token_time'] = time();
-}else{
-    $token = $_SESSION['token'];
-}
+// if (!isset($_SESSION['token'])) {
+//     $token = md5(uniqid(rand(), TRUE));
+//     $_SESSION['token'] = $token;
+//     $_SESSION['token_time'] = time();
+// }else{
+//     $token = $_SESSION['token'];
+// }
 
-define('ADMINEMAIL', 'mads@garderobemekka.no');
+// define('ADMINEMAIL', 'mads@garderobemekka.no');
 
 ?>
